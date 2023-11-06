b5e5aad569427a9a1019393786a9f160d292ef9f
Placardo <20092387@tafe.wa.edu.au>
2023-11-06 14:08:18 +0800

    update

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -3,8 +3,13 @@ import "./services.css";
 
 const Services = () => {
   return (
-    <div className="kat__services">
-      Yes
+    <div className="section__padding">
+      <div className="kat__services-container">
+        <div className="kat__services-title">
+          Our Services
+        </div>
+        
+      </div>
     </div>
   );
 };
