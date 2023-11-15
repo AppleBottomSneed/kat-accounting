afea1bf35a1a2799154abe419e358dae2230a592
Placardo <20092387@tafe.wa.edu.au>
2023-11-15 17:17:31 +0800

    dada

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -9,16 +9,19 @@ const Services = () => {
         <div className="kat__services-title">
           <p>Our Services</p>
         </div>
-          <div className="kat__services-infocard kat__services-container-padding">
-            <div className="kat__services-infocard-feature">
-              <div className="kat__services-infocard-icon">
-                <img src={images.businessIcon} alt="business icon" />
-              </div>
-              <div className="kat__services-infocard-title">
-                <p>Business Advisory</p>
-              </div>
+        <div className="kat__services-infocard kat__services-container-padding">
+          <div className="kat__services-infocard-feature">
+            <div className="kat__services-infocard-icon">
+              <img src={images.businessIcon} alt="business icon" />
+            </div>
+            <div className="kat__services-infocard-title">
+              <p>Business Advisory</p>
             </div>
           </div>
+          <div className="kat__services-infocard-description">
+              <p>We’ll help you develop the right structure for your business that makes sure you are set up in the best way for where you want your business to go. Or if you have a good idea for a new business venture but don’t have expertise in the legal or financial aspects of creating a new business, we can help you:</p>
+          </div>
+        </div>
         
         
         
