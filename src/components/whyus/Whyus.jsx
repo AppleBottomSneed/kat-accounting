081d9f6b0d184af8f9897e04b037984eddfabec1
AppleBottomSneed <20092387@tafe.wa.edu.au>
2023-11-27 16:48:16 +0800

    whyus update

--- src/components/whyus/Whyus.jsx
+++ src/components/whyus/Whyus.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import { images } from "../../constants";
 import "./whyus.css";
 
 const Whyus = () => {
     return (
         <div className="section__padding">
             <div className="kat__whyus-container kat__whyus-container-padding">
-                <p>Hello</p>
+                <div className="kat__whyus-title">
+                    <p>Why Us?</p>
+                </div>
+                <div className="kat__whyus-flexbox">
+                    <div className="kat__whyus-flexbox-container kat__whyus-container-padding">
+                        <div className="kat__whyus-selfie">
+                            <img src={images.serviceIcon1} alt="directorimage"/>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
