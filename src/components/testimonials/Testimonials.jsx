1767dc74b5b62fc8a2f25510b860aeac6647112c
Placardo <20092387@tafe.wa.edu.au>
2023-11-30 21:41:28 +0800

    testi 3

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React from 'react';
+import './testimonials.css';
 
 const Testimonials = () => {
   return (
-    <div>Testimonials</div>
+    <div className="kat__testimonial-bg section__padding">
+      <div className="kat__testimonial-flexbox">
+        <div className="kat__testimonial-title">
+          <p>What Our Clients Say:</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
