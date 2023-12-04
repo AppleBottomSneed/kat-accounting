d7c9d2f0df15c0b5fdf743f5ec02b8e67a48b73e
Placardo <20092387@tafe.wa.edu.au>
2023-12-04 17:14:25 +0800

    dd
    dd

--- src/components/findus/Findus.jsx
+++ src/components/findus/Findus.jsx
@@ -10,8 +10,20 @@ const Findus = () => {
             <p>Come Find Us:</p>
           </div>
           <div className="kat__findus-hours-box">
-            <div className="kat__findus-hours-box-title">
-             <p>Hello</p>
+            <div className="kat__findus-hours-box-info">
+              <div className="kat__findus-hours-box-title">
+                <p>Working Hours</p>
+              </div>
+              <div className="kat__findus-hours-box-group">
+                <div className="kat__findus-hours-box-day">
+                  <p>Mon - Fri</p>
+                </div>
+                <div className="kat__findus-hours-box-time">
+                  <p>9:30 am - 5pm</p>
+                </div>
+              </div>
+
+
             </div>
           </div>
         </div>
