28b302985da5d4d98a6e7c853fdff3540353ac82
Placardo <20092387@tafe.wa.edu.au>
2023-12-05 12:41:13 +0800

    findus update
    before integrating google api

--- src/components/findus/Findus.jsx
+++ src/components/findus/Findus.jsx
@@ -2,35 +2,55 @@ import React from 'react';
 import './findus.css';
 import { images } from '../../constants';
 
+const Workhours = () => (
+  <div className="kat__findus-availability-box">
+    <div className="kat__findus-banner">
+      <p>Come Find Us:</p>
+    </div>
+    <div className="kat__findus-hours-box">
+      <div className="kat__findus-hours-box-info">
+        <div className="kat__findus-hours-box-title">
+          <p>Working Hours</p>
+        </div>
+        <div className="kat__findus-hours-box-group">
+          <div className="kat__findus-hours-box-day">
+            <p>Monday - Friday</p>
+          </div>
+          <div className="kat__findus-hours-box-time">
+            <p>9:30 am - 5pm</p>
+          </div>
+         </div>
+        <div className="kat__findus-hours-box-group">
+          <div className="kat__findus-hours-box-day">
+            <p>Saturday</p>
+          </div>
+          <div className="kat__findus-hours-box-time">
+            <p>Appointment only</p>
+          </div>
+        </div>
+        <div className="kat__findus-hours-box-group">
+          <div className="kat__findus-hours-box-day">
+            <p>Sunday</p>
+          </div>
+          <div className="kat__findus-hours-box-time">
+            <p>Closed</p>
+          </div>
+         </div>
+        <div className="kat__findus-hours-box-asterix">
+          <p>*Call us now on +08 64 603 830</p>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
 const Findus = () => {
   return (
     <div className="kat__findus-container">
-        <div className="kat__findus-availability-box">
-          <div className="kat__findus-banner">
-            <p>Come Find Us:</p>
-          </div>
-          <div className="kat__findus-hours-box">
-            <div className="kat__findus-hours-box-info">
-              <div className="kat__findus-hours-box-title">
-                <p>Working Hours</p>
-              </div>
-              <div className="kat__findus-hours-box-group">
-                <div className="kat__findus-hours-box-day">
-                  <p>Mon - Fri</p>
-                </div>
-                <div className="kat__findus-hours-box-time">
-                  <p>9:30 am - 5pm</p>
-                </div>
-              </div>
-
-
-            </div>
-          </div>
-        </div>
-
-        <div className="kat__findus-googlemaps-box section__padding">
-            hello
-        </div>
+      <Workhours />
+      <div className="kat__findus-googlemaps-box section__padding">
+        hello
+      </div>
     </div>
   )
 }
